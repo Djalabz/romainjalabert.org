74cb0b811733cb08c734c88ae1b6d976c6d19d67
djalabz <djalabz@gmail.com>
2021-05-24 18:28:42 +0200

    menu+firstAnimation

---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+// window.onscroll = function() {getSticky()};
+
+// var header = document.getElementById('mainHeader');
+
+// var sticky = header.offsetTop;
+
+// function getSticky() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
